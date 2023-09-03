9679b5f4ec25c7d04cda6bb21b7066e98f9f4b8b
Nishant Rodrigues <nishantjr@gmail.com>
2023-09-03 18:06:18 -0400

    Initial firmware

---
+++ keyboards/keebio/quefrency/keymaps/njr/rules.mk
@@ -0,0 +1,13 @@
+# Save space and disable unused features
+# ======================================
+
+LTO_ENABLE = yes
+BACKLIGHT_ENABLE = no
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+MUSIC_ENABLE = no
+
+MOUSEKEY_ENABLE = yes
